71c71cfedd587b33628f7d0b2480581ada744d2b
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-31 18:55:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,5 +5,8 @@ class App < Sinatra::Base
   get '/' do 
     "Welcome to your app!!!! I'm trying to make a change!but now im useing shot gun"
   end
-
+  get '/nick' do 
+    "jfjhg
+    "
+  end
 end
